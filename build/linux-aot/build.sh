d77f6a12cd4154371732bed01d2ddd5f310253c3
Warrenn <wenslin@bridgewaybentech.com>
2025-07-17 13:06:32 +0200

    v1.0.0.23

--- build/linux-aot/build.sh
+++ build/linux-aot/build.sh
@@ -14,8 +14,8 @@ mkdir -p linux-aot-output/publish
 # Copy binary from container to host
 docker create --name temp aws-backup-linux-aot
 cp -rf ./aws-backup/cfn ./linux-aot-output/publish
-docker cp temp:/app/aws-backup/bin/Release/net9.0/linux-x64/publish ./linux-aot-output
-docker cp temp:/app/aws-backup-commands/bin/Release/net9.0/linux-x64/publish ./linux-aot-output
-docker cp temp:/app/cert-gen/bin/Release/net9.0/linux-x64/publish ./linux-aot-output
+docker cp temp:/app/aws-backup/publish ./linux-aot-output
+docker cp temp:/app/aws-backup-commands/publish ./linux-aot-output
+docker cp temp:/app/cert-gen/bin/publish ./linux-aot-output
 docker rm temp
 docker system prune -af
--- build/linux/build.sh
+++ build/linux/build.sh
@@ -14,8 +14,8 @@ mkdir -p linux-output/publish
 # Copy binary from container to host
 docker create --name temp aws-backup-linux
 cp -rf ./aws-backup/cfn ./linux-output/publish
-docker cp temp:/app/aws-backup/bin/Release/net9.0/win-x64/publish ./linux-output
-docker cp temp:/app/aws-backup-commands/bin/Release/net9.0/win-x64/publish ./linux-output
-docker cp temp:/app/cert-gen/bin/Release/net9.0/win-x64/publish ./linux-output
+docker cp temp:/app/aws-backup/publish ./linux-output
+docker cp temp:/app/aws-backup-commands/publish ./linux-output
+docker cp temp:/app/cert-gen/publish ./linux-output
 docker rm temp
 docker system prune -af
