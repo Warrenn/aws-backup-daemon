f0ecf8cbc3668c04fb05d51d76d15cb81ac777a4
Warrenn <wenslin@bridgewaybentech.com>
2025-08-25 20:17:50 +0200

    feat: add ssm-param project and publish step to release workflow

---
+++ ssm-param/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+
+var path = args[0];
+if (string.IsNullOrWhiteSpace(path))
+{
+    Console.Error.WriteLine("Path to the parameter is required");
+    return -1;
+}
+
+var ssmClient = new AmazonSimpleSystemsManagementClient();
+var response = await ssmClient.GetParameterAsync(new GetParameterRequest
+{
+    Name = path,
+    WithDecryption = true
+});
+
+Console.WriteLine(response.Parameter.Value);
+return 0;
